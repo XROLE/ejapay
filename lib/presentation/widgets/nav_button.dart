a8697acc96f7e8e75c306b9079f27afd82c7f504
xrole <xrolediamond@gmail.com>
2023-01-27 14:36:03 +0100

    create home page ui create reusable text style create reusable app colors create reusable payment method tile

---
+++ lib/presentation/widgets/nav_button.dart
@@ -0,0 +1,21 @@
+import 'package:ejapay/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class NavButton extends StatelessWidget {
+  final Function() onTap;
+  final Icon? icon;
+  const NavButton({super.key, required this.onTap, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      width: 20,
+      decoration: BoxDecoration(
+        color: AppColors.primary.withOpacity(.2),
+        shape: BoxShape.circle,
+      ),
+      child: icon ?? Icon(Icons.arrow_back_ios, color: AppColors.primaryBlue.withOpacity(.7), size: 13,),
+    );
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color.fromRGBO(175, 183,253, 1);
+  static Color primaryBlue = const Color.fromRGBO(8, 18, 92, 1);
+}
