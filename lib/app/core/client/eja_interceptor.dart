da058c2bd6de672a5cc375d36c07c347e7436f36
xrole <xrolediamond@gmail.com>
2023-02-13 12:07:02 +0100

    abstract header object and make it reusable

--- lib/app/core/client/eja_interceptor.dart
+++ lib/app/core/client/eja_interceptor.dart
@@ -1,16 +1,15 @@
 import 'package:dio/dio.dart';
-import 'package:ejapay/utils/app_logger.dart';
 
 class EjaInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    Response<Map<String, dynamic>> errorRes =
-        Response(requestOptions: options, data: {"message": "Rejected querry request"});
+    // Response<Map<String, dynamic>> errorRes =
+    //     Response(requestOptions: options, data: {"message": "Rejected querry request"});
 
-    DioError sampleError = DioError(
-      requestOptions: options,
-      response: errorRes,
-    );
+    // DioError sampleError = DioError(
+    //   requestOptions: options,
+    //   response: errorRes,
+    // );
 
     // Sample reject
     // handler.reject(sampleError);
@@ -48,6 +47,8 @@ class EjaInterceptor extends Interceptor {
     //   ]
     // ));
 
-    // return handler.next(response);
+    super.onResponse(response, handler);
   }
+
+  
 }
