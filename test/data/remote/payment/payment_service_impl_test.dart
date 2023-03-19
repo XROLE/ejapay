2e6ff1bbe80a8638fa4d55aee070b3fdd49d884b
xrole <xrolediamond@gmail.com>
2023-03-19 13:55:22 +0100

    fix get payment methods failling test

--- test/data/remote/payment/payment_service_impl_test.dart
+++ test/data/remote/payment/payment_service_impl_test.dart
@@ -8,44 +8,53 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
+import '../../../core_mock/mock_payment_methods.dart';
 import '../../../core_mock/mock_wallet.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  late MockHttpClient mockHttpClient;
+  late MockHttpClient? mockHttpClient;
   late PaymentServiceImpl sut;
 
   setUp(() {
     mockHttpClient = MockHttpClient();
-    sut = PaymentServiceImpl(mockHttpClient);
+    sut = PaymentServiceImpl(mockHttpClient!);
+  });
+
+  tearDown(() {
+    mockHttpClient = null;
   });
 
   group("PaymentServiceImplementation", () {
     const mockMethodId = 1;
-    final String url = Endpoints.getPaymentSettings(mockMethodId);
     const String mockToken = "token";
     final List<Map<String, dynamic>> mockWalletList = [walletJson];
+    List<Map<String, dynamic>> paymentMethodList = [paymentMethodJson];
 
     Map<String, String> headers = reqHeaders;
     reqHeaders['authorization'] = "Bearer $mockToken";
 
     test("get payment settings", () async {
-      when(() => mockHttpClient.get(url, headers: headers)).thenAnswer((_) => Future.value(
+    final String url = Endpoints.getPaymentSettings(mockMethodId);
+
+      when(() => mockHttpClient!.get(url, headers: headers)).thenAnswer((_) => Future.value(
           Response(requestOptions: RequestOptions(path: url), data: {"data": mockWalletList})));
 
       final List<WalletModel> data =
           await sut.getPaymentSettings(token: mockToken, methodId: mockMethodId);
 
       expect(data, isA<List<WalletModel>>());
-      verify(() => mockHttpClient.get(url, headers: headers)).called(1);
+      verify(() => mockHttpClient!.get(url, headers: headers)).called(1);
     });
 
     test("get payment methods", () async {
-      when(() => mockHttpClient.get(url, headers: headers)).thenAnswer(
-          (_) => Future.value(Response(requestOptions: RequestOptions(path: url), data: [])));
+    const String url = Endpoints.getPaymentMethods;
+
+      when(() => mockHttpClient!.get(url, headers: headers)).thenAnswer((_) => Future.value(
+          Response(requestOptions: RequestOptions(path: url), data: {"data": paymentMethodList})));
 
       await sut.getPaymentMethods(mockToken);
-      verify(() => mockHttpClient.get(url, headers: headers)).called(1);
+      verify(() => mockHttpClient!.get(url, headers: headers)).called(1);
     });
   });
 }
